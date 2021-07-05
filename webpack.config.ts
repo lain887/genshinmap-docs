9521f40a10e3ee3e2b9c1b09868f68e882a9d282
(^_^) <jiazengp@gmail.com>
2021-07-05 15:49:08 +0800

    docs(agreement.md): update

--- webpack.config.ts
+++
@@ -1,7 +0,0 @@
-import type webpack from 'webpack'
-
-const config: webpack.Configuration = {
-  mode: process.env.NODE_ENV == 'production' ? 'production' : 'development',
-}
-
-export default config
